a8606fe5101958360e40f437138d3ae86c7f71fb
Javed Qadrud-Din <jqjunk@MacBook-Pro-3.local>
2017-03-28 18:12:00 -0600

    first commit

---
+++ lib/scraper.py
@@ -0,0 +1,53 @@
+import sys
+import xml.etree.ElementTree as ET
+import urllib2
+import csv
+from os import path
+
+SEC_SEARCH_URL = "https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK=%s&count=100&type=S-1&output=xml"
+OUTPUT_PATH = "../data/s-1_filing_data"
+
+with open(sys.argv[1]) as csvfile:
+    reader = csv.DictReader(csvfile)
+    failed_to_get_filings = 0
+    failed_to_get_txt_file = 0
+    got_txt_file = 0
+    total_companies_tried = 0
+    for row in reader:
+        total_companies_tried += 1
+        ticker = row['Symbol']
+        print "looking up ticker: " + ticker
+
+        #getting a link to the S-1 by searching on the SEC website
+        try:
+            site = urllib2.urlopen(SEC_SEARCH_URL % ticker)
+            data = site.read()
+            root = ET.fromstring(data)
+            results = root.findall('results')
+            filings = results[0].findall('filing')
+            link = filings[-1].find('filingHREF').text
+        except:
+            print("failed to get filings")
+            failed_to_get_filings += 1
+            continue
+
+        #get the text of the s-1 directly instead of going to the index page
+        link = link.split('-index')[0] + '.txt'
+
+        filename = ticker + '_S-1.txt'
+        try:
+            s1_site = urllib2.urlopen(link)
+            with open(path.join(OUTPUT_PATH, filename), 'w') as f:
+                f.write(s1_site.read())
+            #os.system('wget -O ' + filename + ' ' + link)
+            got_txt_file += 1
+        except:
+            print("failed to get txt file")
+            failed_to_get_txt_file += 1
+
+    print("Total companies tried: " + str(total_companies_tried))
+    print("Failed to get filings list: " + str(failed_to_get_filings))
+    print("Failed to get text file: " + str(failed_to_get_txt_file))
+    print("Succeeded in getting text file: " + str(got_txt_file))
+    success_fail_test = total_companies_tried == failed_to_get_filings + failed_to_get_txt_file + got_txt_file
+    print("Total failures plus total successes adds up to total tries? " + str(success_fail_test))
